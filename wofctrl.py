7ea812cc23726d42473de7ed50302af1bed307a5
John Porterfield <jporterfield@bridgesourcemedical.com>
2023-07-10 20:15:08 -0500

    Implemented word wrap

--- wofctrl.py
+++ wofctrl.py
@@ -1,9 +1,7 @@
 import pygame
 import sys
-import itertools 
 
 black = (0,0,0)
-red = (255,0,0)
 white = (255,255,255)
 
 class Window(object):
@@ -22,14 +20,45 @@ class Window(object):
   def addRect(self, rectx, recty):
     self.rect = pygame.draw.rect(self.window, white, (rectx, recty, self.rectwidth, self.rectheight))
 
-  def addText(self, rectx, recty):
-    self.window.blit(self.font.render('E', True, red), (rectx, recty+1))
+  def addText(self, rectx, recty, letter):
+    # Adds a letter to the board at the position rectx, recty, where 0, 0 is the top left, 
+    # centered on the background rectangle
+    text = self.font.render(letter, True, black)
+    textrect = text.get_rect(center = (rectx + self.rectwidth/2, recty + self.rectheight/2))
+    self.window.blit(text, textrect)
 
-  def addLetter(self, row, col):
+  def addLetter(self, row, col, letter):
     # Adds a letter at positions on the wheel of fortune board in the format x, y, 
     # where 0, 0 is the top left, and 4, 12 is the bottom right. 
     self.addRect(53 + col*(Wm.rectwidth + Wm.buf), 61+ row*(Wm.rectheight + Wm.buf))
-    self.addText(53 + col*(Wm.rectwidth + Wm.buf), 61+ row*(Wm.rectheight + Wm.buf))
+    self.addText(53 + col*(Wm.rectwidth + Wm.buf), 61+ row*(Wm.rectheight + Wm.buf), letter)
+
+  def addWords(self, letters):
+    # Adds a list of letters to the board, starting at position startrow, startcol
+    # Tokenizes the string to determine how many words will fit on a row of length 12
+    # and then adds the words to the board, starting at column zero for new words
+    # and starting at the end of the previous word for words that continue from the
+    # previous line.
+    # TODO: Add a check to see if the word will fit on a new line, and if not,
+    # hyphenate it and add the rest to the next line
+
+    startrow = 0
+    startcol = 0
+    letters = letters.upper()
+    words = letters.split()
+    for word in words:
+      if startcol + len(word) > 12:
+        startrow += 1
+        startcol = 0
+      for letter in word:
+        self.addLetter(startrow, startcol, letter)
+        startcol += 1
+      startcol += 1
+
+  def clearBoard(self):
+    # Clears the board of all letters
+    self.window.fill(black)
+    self.addBg()
 
   def addBg(self):
     self.window.blit(self.bgimg, (0,0))
@@ -37,15 +66,11 @@ class Window(object):
 
 if __name__ == '__main__':
   run = True
-
   Wm = Window()
-  Wm.addBg()
-  Wm.addLetter(0, 0)
-  Wm.addLetter(1, 1)
-  Wm.addLetter(1, -1)
-  Wm.addLetter(2, 2)
-  Wm.addLetter(3, 3)
-  Wm.addLetter(3, 11)
+  Wm.clearBoard()
+  Wm.addWords("Gaius Baltar is the Motherfucking Shit")
+  Wm.clearBoard()
+  Wm.addWords("Hello World!")
   pygame.display.flip()
   while run:
       for event in pygame.event.get():
