d08b66c43479d80f84c0ccde3536a894bf55fac5
John Porterfield <jporterfield@bridgesourcemedical.com>
2023-07-13 23:23:32 -0500

    Creates 3 player obscured phrase game

--- wofctrl.py
+++ wofctrl.py
@@ -42,6 +42,112 @@ class WOFHumanPlayer(WOFPlayer):
     
     choose = (input("Guess a letter, phrase, or type 'exit' or 'pass':"))
     return choose
+class WOFComputerPlayer(WOFPlayer):
+  SORTED_FREQUENCIES = "ZQXJKVBPYGFWMUCLDRHSNIOATE"
+  
+  def __init__(self, name, difficulty ):
+    WOFPlayer.__init__(self, name)
+    self.difficulty = difficulty
+      
+  def smartCoinFlip(self):                 # give difficulty level
+    if random.randint(1, 10) <= self.difficulty:
+      return False
+    else:
+      return True 
+      
+  def getPossibleLetters(self, guessed):
+    will_guess = []
+    
+    for i in LETTERS:
+      if (i not in guessed) and (i not in VOWELS) and (i in LETTERS):
+        will_guess.append(i)
+      elif (i not in guessed) and (i in VOWELS):
+        if self.prizeMoney > VOWEL_COST:
+          will_guess.append(i)
+    return will_guess
+  
+  def getMove(self, category, obscuredPhrase, guessed):
+    will_guess = self.getPossibleLetters(guessed)
+    
+    if will_guess == []:
+      return 'pass'
+    else:
+      if self.smartCoinFlip() is True:
+        for i in self.SORTED_FREQUENCIES[::-1]:
+          if i in will_guess:
+            return i
+      else:
+        return random.choice(will_guess)
+  
+# Spins the wheel of fortune wheel to give a random prize
+# Examples:
+#    { "type": "cash", "text": "$950", "value": 950, "prize": "A trip to Ann Arbor!" },
+#    { "type": "bankrupt", "text": "Bankrupt", "prize": false },
+#    { "type": "loseturn", "text": "Lose a turn", "prize": false }
+def spinWheel():
+  with open("wheel.txt", 'r') as f:
+    wheel = json.loads(f.read())
+    return random.choice(wheel)
+
+# Returns a category & phrase (as a tuple) to guess
+# Example:
+#     ("Artist & Song", "Whitney Houston's I Will Always Love You")
+def getRandomCategoryAndPhrase():
+  with open("phrases.txt", 'r') as f:
+    phrases = json.loads(f.read())
+
+    category = random.choice(list(phrases.keys()))
+    phrase   = random.choice(phrases[category])
+    return (category, phrase.upper())
+    
+# Given a phrase and a list of guessed letters, returns an obscured version
+# Example:
+#     guessed: ['L', 'B', 'E', 'R', 'N', 'P', 'K', 'X', 'Z']
+#     phrase:  "GLACIER NATIONAL PARK"
+#     returns> "_L___ER N____N_L P_RK"
+def obscurePhrase(phrase, guessed):
+  rv = ''
+  for s in phrase:
+    if (s in LETTERS) and (s not in guessed):
+      rv = rv+'_'
+    else:
+      rv = rv+s
+  return rv
+
+# num_human = getNumberBetween('How many human players?', 0, 10)
+num_human = 1
+
+# Create the human player instances
+human_players = [WOFHumanPlayer('Player 1')]
+
+# num_computer = getNumberBetween('How many computer players?', 0, 10)
+num_computer = 2
+
+# If there are computer players, ask how difficult they should be
+if num_computer >= 1:
+    # difficulty = getNumberBetween('What difficulty for the computers? (1-10)', 1, 10)
+    difficulty = 10
+
+# Create the computer player instances
+computer_players = [WOFComputerPlayer('Computer {}'.format(i+1), difficulty) for i in range(num_computer)]
+
+players = human_players + computer_players
+
+# No players, no game :(
+if len(players) == 0:
+    print('We need players to play!')
+    raise Exception('Not enough players')
+
+# category and phrase are strings.
+category, phrase = getRandomCategoryAndPhrase()
+# guessed is a list of the letters that have been guessed
+guessed = []
+
+# playerIndex keeps track of the index (0 to len(players)-1) of the player whose turn it is
+playerIndex = 0
+
+# will be set to the player instance when/if someone wins
+winner = False
 
 class Window(object):
   def __init__(self):
@@ -107,7 +213,8 @@ if __name__ == '__main__':
   run = True
   Wm = Window()
   Wm.clearBoard()
-  Wm.addWords("Jim Ca__ey As Ace Ventu_a")
+  obscuredPhrase = obscurePhrase(phrase, guessed)
+  Wm.addWords(obscuredPhrase)
   pygame.display.flip()
   while run:
       for event in pygame.event.get():
