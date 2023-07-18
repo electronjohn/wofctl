ff5c208f4206a9fc0bfbcd9d473e6bfb31419724
John Porterfield <johnporterfield@gmail.com>
2023-07-18 17:00:51 -0500

    made grid generation pythonic with numpy

--- ctrl.py
+++ ctrl.py
@@ -15,24 +15,21 @@ screen = pygame.display.set_mode(size)  # Create the game window with the size (
 font = pygame.font.SysFont('Arial', 60)  # Create a Font object for the timer text
 
 # Set image coordinates
-Xpts = [0, 64, 128, 192, 256, 320]  # x coordinates
-Ypts = [0, 30, 60, 90, 120, 150]  # y coordinates
-# map = np.array(list(itertools.product(Xpts, Ypts)))  # 25 image coordinates
+Xpts = np.linspace(0,width,6)  # x coordinates
+Ypts = np.linspace(0,height-90,6)  # y coordinates
 ha = itertools.product(Xpts, Ypts)  # Generate all permutations of x and y coordinates, resulting in 25 sets of data
 haha = list(ha)  # Convert the permutations to a list
-'''Result: [(0, 0), (0, 48), (0, 96), (0, 144), (0, 192), (48, 0), (48, 48), (48, 96), (48, 144),
- (48, 192), (96, 0), (96, 48), (96, 96), (96, 144), (96, 192), (144, 0), (144, 48), (144, 96),
- (144, 144), (144, 192), (192, 0), (192, 48), (192, 96), (192, 144), (192, 192)]'''
+'''
+Result: [( 0.0 , 0.0 ), ( 0.0 , 30.0 ), ( 0.0 , 60.0 ), ( 0.0 , 90.0 ), ( 0.0 , 120.0 ), ( 0.0 , 150.0 ), ( 64.0 , 0.0 ), ( 64.0 , 30.0 ), ( 64.0 , 60.0 ), ( 64.0 , 90.0 ), ( 64.0 , 120.0 ), ( 64.0 , 150.0 ), ( 128.0 , 0.0 ), ( 128.0 , 30.0 ), ( 128.0 , 60.0 ), ( 128.0 , 90.0 ), ( 128.0 , 120.0 ), ( 128.0 , 150.0 ), ( 192.0 , 0.0 ), ( 192.0 , 30.0 ), ( 192.0 , 60.0 ), ( 192.0 , 90.0 ), ( 192.0 , 120.0 ), ( 192.0 , 150.0 ), ( 256.0 , 0.0 ), ( 256.0 , 30.0 ), ( 256.0 , 60.0 ), ( 256.0 , 90.0 ), ( 256.0 , 120.0 ), ( 256.0 , 150.0 ), ( 320.0 , 0.0 )]
+'''
 map = np.array(haha)  # Convert the list data to an array
-
-print(map)
 
 # Define the ready function to determine the image sequence
 def ready():
 	global list1  # Define a global variable list1
-	list1 = ['A', 'B', 'C', 'D', 'E', 'Z', 'F', 'G', 'H', 'I', 'J', '_', 'K', 'L'
-	  					, 'M', 'N', 'O', '_', 'P', 'Q', 'R', 'S', 'T', '[', 'U', 'V'
-							, 'W', 'X', 'Y', 'WHL']  # Define a list of 36 letters
+	list1 = ['A', 'B', 'C', 'D', 'E', 'Z', 'F', 'G', 'H', 'I', 'J', ' ', 'K', 'L'
+	  					, 'M', 'N', 'O', ' ', 'P', 'Q', 'R', 'S', 'T', '[', 'U', 'V'
+							, 'W', 'X', 'Y', '\r\n']  # Define a list of 3 letters
 
 # Define the game interface
 def gamePage(game_page):
@@ -42,7 +39,7 @@ def gamePage(game_page):
 			if event.type == pygame.QUIT:  # If the window is closed, quit
 				pygame.quit()  # Quit pygame
 				sys.exit()
-			for i in range(36):  # Loop 36 times
+			for i in range(31):  # Loop 31) times
 				# If the mouse is released and within the range of a certain letter image
 				if event.type == pygame.MOUSEBUTTONUP and map[i][0] <= event.pos[0] <= map[i][0] + 64 and map[i][1] <= event.pos[1] <= map[i][1] + 30:
 					print(*list1[i])  # Print the letter corresponding to the clicked position
